9f4404c7e36fffeaf22f0205ecacdfca53925d82
Murat Toygar <toygar-murat@hotmail.com>
2019-10-12 22:14:17 +0300

    include rails filter_params to filters unless explicitly configured otherwise

--- lib/sidekiq-crypt.rb
+++ lib/sidekiq-crypt.rb
@@ -6,12 +6,11 @@ require 'sidekiq-crypt/server_middleware'
 module Sidekiq
   module Crypt
     class << self
-      def configuration
-        @configuration ||= Configuration.new
-      end
+      def configure(options = {})
+        @configuration = Configuration.new(options)
+        yield(@configuration) if block_given?
 
-      def configure
-        yield(configuration) if block_given?
+        raise 'you must specify at least one filter' if @configuration.filters.blank?
 
         inject_sidekiq_middlewares
       end
@@ -19,17 +18,17 @@ module Sidekiq
       def inject_sidekiq_middlewares
         ::Sidekiq.configure_client do |config|
           config.client_middleware do |chain|
-            chain.add Sidekiq::Crypt::ClientMiddleware, configuration: configuration
+            chain.add Sidekiq::Crypt::ClientMiddleware, configuration: @configuration
           end
         end
 
         ::Sidekiq.configure_server do |config|
           config.client_middleware do |chain|
-            chain.add Sidekiq::Crypt::ClientMiddleware, configuration: configuration
+            chain.add Sidekiq::Crypt::ClientMiddleware, configuration: @configuration
           end
 
           config.server_middleware do |chain|
-            chain.add Sidekiq::Crypt::ServerMiddleware, configuration: configuration
+            chain.add Sidekiq::Crypt::ServerMiddleware, configuration: @configuration
           end
         end
       end
--- lib/sidekiq-crypt/configuration.rb
+++ lib/sidekiq-crypt/configuration.rb
@@ -9,6 +9,16 @@ module Sidekiq
         @encryption_class = DefaultCipher::Encrypt
         @decryption_class = DefaultCipher::Decrypt
         @filters = []
+
+        include_rails_filter_parameters(options)
+      end
+
+      private
+
+      def include_rails_filter_parameters(options)
+        return unless defined?(::Rails) && (options[:include_rails_filters] || true)
+
+        @filters = ::Rails.application.config.filter_parameters
       end
     end
   end
